48d14c5a61e54913d90ee417e7658a2eec1ad319
andrew kim <andrewkim001@yahoo.com>
2024-10-01 15:54:03 -0700

    wokring/ kinda finished working on modules for the applications

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,44 +1,41 @@
-/** 
+/**
  * API for OpenWeather (https://openweathermap.org/)
  */
-'use strict';
+"use strict";
 
 const api_key = config.MY_KEY;
 
-
 /**
- * 
- * @param {*} URL 
- * @param {*} callback 
+ * Fetch data from server
+ * @param {string} URL API url
+ * @param {function} callback callback
  */
 
-
-export const fetchData = function(URL, callback) {
-    fetch('${URL}&appid=${api_key')
-        .then(res => res.json())
-        .then(data => callback(data));
-}
+export const fetchData = function (URL, callback) {
+  fetch("${URL}&appid=${api_key")
+    .then((res) => res.json())
+    .then((data) => callback(data));
+};
 
 export const url = {
-    currentWeather(lat,lon) {
-        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=imperial`
-    },
-    forecast(lat, lon) {
-        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=imperial`
-    },
-    airPollution(lat,lon) {
-        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
-    },
-    reverseGeo(lat, lon) {
-        return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
-    },
+  currentWeather(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=imperial`;
+  },
+  forecast(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=imperial`;
+  },
+  airPollution(lat, lon) {
+    return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`;
+  },
+  reverseGeo(lat, lon) {
+    return `http://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`;
+  },
 
-    /**
-     * @param {string} query Search query e.g.: "Torrance", "New York" 
-     * @returns 
-     */
-    geo(query) {
-        return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
-    }
-
-}
+  /**
+   * @param {string} query Search query e.g.: "Torrance", "New York"
+   * @returns
+   */
+  geo(query) {
+    return `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`;
+  },
+};
--- assets/js/module.js
+++ assets/js/module.js
@@ -1,2 +1,97 @@
-/** 
+/**
+ * All module functions
  */
+"use strict";
+
+export const weekDayNames = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Friday",
+  "Saturday",
+];
+
+export const monthNames = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+/**
+ *
+ * @param {number} dateUnix Unix date in secs
+ * @param {number} timezone Timezone shift from UTC in secs
+ * @returns {string} returns the date as a string (format): "Tuesday, 1, Sep"
+ */
+
+export const getDate = function (dateUnix, timezone) {
+  const date = new Date((dateUnix + timezone) * 1000);
+  const weekDayNames = weekDayNames[date.getUTCDay()];
+  const monthName = monthNames[DataTransfer.getUTCMonth()];
+
+  return `${weekDayNames} ${date.getUTCDate}, ${monthName}`;
+};
+
+/**
+ *
+ * @param {number} timeUnix Date in secs
+ * @param {number} timezone Timezone shift from UTC in secs
+ * @returns {string} Returns time as a string (format): HH AM/PM
+ */
+
+export const getTime = function (timeUnix, timezone) {
+  const date = new Date((timeUnix + timezone) * 1000);
+  const hours = date.getUTCHours();
+  const period = hours >= 12 ? "PM" : "AM";
+
+  return `${hours % 12 || 12} ${period}`;
+};
+
+/**
+ *
+ * @param {number} mps meter per sec
+ * @returns {number} Returns Kilometer per hour
+ */
+
+export const mps_to_kmh = (mps) => {
+  const mph = mps * 3600;
+  return mph / 1000;
+};
+
+export const apiText = {
+  1: {
+    level: "Good",
+    message:
+      "Air quality is considered satisfactory, and air pollution posese little or no risk",
+  },
+  2: {
+    level: "Fair",
+    message:
+      "Air quality is acceptable; however for some pollutants there may be a moderate health \
+        concern for a very small number of people who are unusually sensitive to air pollution.",
+  },
+  3: {
+    level: "Moderate",
+    message:
+      "Members of sensitive groups may experience health effects. The general public is not likely to be affected.",
+  },
+  4: {
+    level: "Poor",
+    message:
+      "Everyone may begin to experience health effects; members of sensitive groups may experience more serious health effects.",
+  },
+  5: {
+    level: "Very Poor",
+    message:
+      "Health warnings of emergency conditions. The entire population is more likely to be affected.",
+  },
+};
