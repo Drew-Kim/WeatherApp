a14e8d8c5df03aca5ceee6f1b051c9026b76eb76
andrew kim <andrewkim001@yahoo.com>
2024-10-01 16:03:04 -0700

    working on setting up the routes

--- assets/js/route.js
+++ assets/js/route.js
@@ -1,2 +1,12 @@
 /**
+ * Manage all the routes
  */
+
+'use strict'
+
+import {updateWeather, error404} from "./app.js";
+const defaultLocation = "#/weather?lat=33.839449&lon=-118.349201" // Torrance
+
+const currentLocation = function() {
+    
+}
