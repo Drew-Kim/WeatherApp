035ce831c851ba38f85cecf5e0f97c0bdb7a8f24
andrew kim <andrewkim001@yahoo.com>
2024-10-10 11:00:01 -0700

    Implement a working loading bar The user will see a loading circle when searching for a location and clicking on their selection. The loading circle should disappear when the data from the API is displayed on the page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -80,3 +80,90 @@ searchField.addEventListener("input", function () {
 });
 
 const container = document.querySelector("[data-container]");
+const loading = document.querySelector("[data-loading]");
+const currentLocationBtn = document.querySelector("[data-current-location-btn]");
+const errorContent = document.querySelector("[data-error-content]");
+/**
+ * Load the weather data on page
+ * @param {number} lat 
+ * @param {number} lon
+ */
+
+// Update weather function
+export const updateWeather = function (lat, lon) {
+  loading.style.display = "grid";
+
+  container.style.overflowY = "hidden";
+  container.classList.contains("fade-in") ?? container.classList.remove("fade-in");
+  errorContent.style.display = "none";
+
+  const currentWeatherSection = document.querySelector("[data-current-weather]");
+  const highlightSection = document.querySelector("[data-highlights]");
+  const hourlySection = document.querySelector("[data-hourly-forecast]");
+  const forecastSection = document.querySelector("[data-5-day-forecast]");
+
+  currentWeatherSection.innerHTML = "";
+  highlightSection.innerHTML = "";
+  hourlySection.innerHTML = "";
+  forecastSection.innerHTML = "";
+
+  if (window.location.hash === "#/current-lcoation"){
+    currentLocationBtn.setAttribute("disabled", "");
+  } else {
+    currentLocationBtn.removeAttribute("disabled");
+  }
+  // Current Weather
+  fetchData(url.currentWeather(lat, lon), function (currentWeather) {
+
+    const {
+      weather,
+      dt: dateUnix,
+      sys: {sunrise: sunriseUnixUTC, sunset: sunsetUnixUTC},
+      main: {temp, feels_like, pressure, humidity},
+      visibility,
+      timezone
+    } = currentWeather
+    const [{description, icon}] = weather;
+
+    const card = document.createElement("div");
+    card.classList.add("card", "card-lg", "current-weather-card");
+
+    card.innerHTML = `
+      <h2 class="title-2 card-title">Now</h2>
+        <!-- wrapper for degrees -->
+        <div class="weapper">
+          <p class="heading">${parseInt(temp)}<sup>&deg;F</sup></p>
+          <img
+            src="./assets/images/weather_icons/${icon}.png"
+            width="64"
+            height="64"
+            alt="${description}"
+            class="weather-icon"
+          />
+        </div>
+        <p class="body-3">${description}</p>
+        <ul class="meta-list">
+          <!-- Date -->
+          <li class="meta-item">
+            <span class="fa-regular fa-calendar"></span>
+            <p class="title-3 meta-text">${module.getDate(dateUnix, timezone)}</p>
+          </li>
+          <!-- Location -->
+          <li class="meta-item">
+            <span class="fa-solid fa-location-dot"></span>
+            <p class="title-3 meta-text" data-location></p>
+          </li>
+        </ul>
+    `;
+
+    fetchData(url.reverseGeo(lat, lon), function([{ name, country}]){
+      card.querySelector("[data-location]").innerHTML= `${name}, ${country}`;
+
+    });
+    currentWeatherSection.appendChild(card);
+
+  });
+  
+}
+// Error 404 function
+export const error404 = function(){}
